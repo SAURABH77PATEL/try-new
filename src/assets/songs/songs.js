140dba3eb1f1f5ecf38fddea56f5628044e7b1bf
singal <patelsingal6@gmail.com>
2023-05-25 20:00:58 +0530

    first commit

---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./CalmDownCalmDown.mp3";
+import music2 from "./MaanMeriJaan.mp3";
+import music3 from "./JaiShriRam.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
